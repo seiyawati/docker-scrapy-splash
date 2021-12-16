8349ed8faa851cbfa8f9447378c5ad8af7680fab
Seiya Kawamoto <kid960805@gmail.com>
2021-12-16 17:52:23 +0900

    add: mysql docker env

---
+++ docker/db/sql/001-create-tables.sql
@@ -0,0 +1,12 @@
+---- drop ----
+DROP TABLE IF EXISTS `buyma`;
+
+---- create ----
+create table IF not exists `buyma`
+(
+ `id`               INT(20) AUTO_INCREMENT,
+ `name`             VARCHAR(20) NOT NULL,
+ `created_at`       Datetime DEFAULT NULL,
+ `updated_at`       Datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
