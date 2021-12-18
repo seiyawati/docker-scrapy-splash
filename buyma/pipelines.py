1a0298f30318abb8a2369b54fef81c21a1506880
Seiya Kawamoto <kid960805@gmail.com>
2021-12-18 14:38:16 +0900

    fix: pipelines

--- buyma/pipelines.py
+++ buyma/pipelines.py
@@ -8,31 +8,33 @@
 import pymysql
 
 class BuymaPipeline(object):
-    def open_spider(self, spider):
-        self.connection = pymysql.connect(
-            host="localhost",
-            user="buyma", # DBにあわせて変更
-            passwd="buyma", # DBにあわせて変更
-            database="buyma",
-            charset="utf8mb4"
-        )
-        self.cursor = self.connection.cursor()
-
     def process_item(self, item, spider):
-        # duplication check
-        check_title_id = item["title"]
-        find_qry = "SELECT `title` FROM `books` WHERE `title` = %s"
-        is_done = self.cursor.execute(find_qry, check_title_id)
-
-        # if already a record exists in database, return 1
-        if is_done == 0:
-            insert_qry = "INSERT INTO `books` (`title`, `price`, `detail_page_url`) VALUES (%s, %s, %s)"
-            self.cursor.execute(insert_qry, (item["title"], item["price"], item["detail_page_url"]))
-            self.connection.commit()
-        else:
-            pass
-
         return item
+    # def open_spider(self, spider):
+    #     self.connection = pymysql.connect(
+    #         host="localhost",
+    #         user="buyma",
+    #         passwd="buyma",
+    #         database="buyma",
+    #         charset="utf8mb4"
+    #     )
+    #     self.cursor = self.connection.cursor()
 
-    def close_spider(self, spider):
-        self.connection.close()
+    # def process_item(self, item, spider):
+    #     # duplication check
+    #     check_title_id = item["name"]
+    #     find_qry = "SELECT `name` FROM `products` WHERE `name` = %s"
+    #     is_done = self.cursor.execute(find_qry, check_title_id)
+
+    #     # if already a record exists in database, return 1
+    #     if is_done == 0:
+    #         insert_qry = "INSERT INTO `products` (`name`, `price`, `url`) VALUES (%s, %s, %s)"
+    #         self.cursor.execute(insert_qry, (item["name"], item["price"], item["url"]))
+    #         self.connection.commit()
+    #     else:
+    #         pass
+
+    #     return item
+
+    # def close_spider(self, spider):
+    #     self.connection.close()
