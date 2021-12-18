c02ac90974c43fec10f66c042ad4b1a21f3652ee
Seiya Kawamoto <kid960805@gmail.com>
2021-12-18 12:00:15 +0900

    feat: parse処理

--- buyma/spiders/buyma_spider.py
+++ buyma/spiders/buyma_spider.py
@@ -6,24 +6,24 @@ from ..items import Buyma
 
 class BuymaSpiderSpider(scrapy.Spider):
     name = 'buyma_spider'
-    allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com']
+    allowed_domains = ['buyma.com']
+    start_urls = ['https://www.buyma.com/r/']
 
     def parse(self, response):
-        books = response.xpath('.//*[@class="product_pod"]')
-        for book in books:
+        posts = response.xpath('.//*[@class="product "]')
+        for post in posts:
             item = Buyma()
 
-            item["title"] = book.xpath('.//h3/a/@title').get()
-            item["price"] = book.xpath('.//*[@class="price_color"]/text()').get()
-            img_url = book.xpath('.//*[@class="image_container"]/a/@href').get()
-            item["detail_page_url"] = "http://books.toscrape.com/" + img_url
+            item["name"] = post.xpath('.//div[@class="product_name"]/a/text()').get()
+            item["price"] = post.xpath('.//span[@class="Price_Txt"]/text()').get()
+            url = post.xpath('.//div[@class="product_name"]/a/@href').get()
+            item["url"] = "https://www.buyma.com" + url
 
             yield item
 
 
         # If there is a next button on this page, move the crawler
-        next_page_url = response.xpath('//a[text()="next"]/@href').get()
+        next_page_url = response.xpath('//a[@rel="next"]/@href').get()
         abs_next_page_url = response.urljoin(next_page_url)
         if abs_next_page_url is not None:
             yield Request(abs_next_page_url, callback=self.parse)
