cc9dee7ee33808d5b9a22c4559073dfa760d3611
Seiya Kawamoto <kid960805@gmail.com>
2021-12-18 14:37:04 +0900

    add: books spider

---
+++ buyma/spiders/books_spider.py
@@ -0,0 +1,27 @@
+from scrapy import Spider
+from scrapy.http import Request
+from ..items import Books
+
+class BooksSpiderSpider(Spider):
+    name = 'books_spider'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com']
+
+    def parse(self, response):
+        books = response.xpath('.//*[@class="product_pod"]')
+        for book in books:
+            item = Books()
+
+            item["title"] = book.xpath('.//h3/a/@title').get()
+            item["price"] = book.xpath('.//*[@class="price_color"]/text()').get()
+            img_url = book.xpath('.//*[@class="image_container"]/a/@href').get()
+            item["detail_page_url"] = "http://books.toscrape.com/" + img_url
+
+            yield item
+
+
+        # If there is a next button on this page, move the crawler
+        next_page_url = response.xpath('//a[text()="next"]/@href').get()
+        abs_next_page_url = response.urljoin(next_page_url)
+        if abs_next_page_url is not None:
+            yield Request(abs_next_page_url, callback=self.parse)
