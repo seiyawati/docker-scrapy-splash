2a2789f67ef5e8c01a28fd7c4c8d25865fbcacab
Seiya Kawamoto <kid960805@gmail.com>
2021-12-22 19:30:13 +0900

    add: celine china spider

---
+++ buyma/spiders/celine_china.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CelineChinaSpider(scrapy.Spider):
+    name = 'celine_china'
+    allowed_domains = ['www.celine.cn']
+    start_urls = ['http://www.celine.cn/']
+
+    def parse(self, response):
+        pass
