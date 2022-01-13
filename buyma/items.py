d39dddf83c5ddf3a353dbc6b05f60470274c4cc6
Seiya Kawamoto <kid960805@gmail.com>
2022-01-13 16:07:41 +0900

    fix

--- buyma/items.py
+++ buyma/items.py
@@ -5,26 +5,21 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Field, Item
 
 
-class Buyma(scrapy.Item):
-    name = scrapy.Field()
-    price = scrapy.Field()
-    url = scrapy.Field()
+class Buyma(Item):
+    name = Field()
+    price = Field()
+    url = Field()
 
-class Rakuten(scrapy.Item):
-    name = scrapy.Field()
-    price = scrapy.Field()
-    brand = scrapy.Field()
+class Books(Item):
+    title = Field()
+    price = Field()
+    detail_page_url = Field()
 
-class Books(scrapy.Item):
-    title = scrapy.Field()
-    price = scrapy.Field()
-    detail_page_url = scrapy.Field()
-
-class ProductItem(scrapy.Item):
-    url = scrapy.Field()
-    name = scrapy.Field()
-    price = scrapy.Field()
-    image = scrapy.Field()
+class ProductItem(Item):
+    url = Field()
+    name = Field()
+    price = Field()
+    image = Field()
